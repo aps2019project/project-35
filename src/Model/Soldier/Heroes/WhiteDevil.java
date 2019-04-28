730506785825b3cebd3fe274e04be1a887638208
doctorhoseinpour <45894816+doctorhoseinpour@users.noreply.github.com>
2019-04-28 15:58:26 +0430

    WhiteDevil Completed

--- src/Model/Soldier/Heroes/WhiteDevil.java
+++ src/Model/Soldier/Heroes/WhiteDevil.java
@@ -3,4 +3,7 @@ package Model.Soldier.Heroes;
 import Model.Soldier.MeleeHero;
 
 public class WhiteDevil extends MeleeHero {
+    public WhiteDevil(){
+        this.hp = 50;
+    }
 }
--- src/Model/Soldier/Soldier.java
+++ src/Model/Soldier/Soldier.java
@@ -3,4 +3,6 @@ package Model.Soldier;
 import Model.Card;
 
 public class Soldier extends Card {
+    protected int hp;
+    protected int ap;
 }
